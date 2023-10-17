e8271e1b804b4789aa9d9713b74c573aa5b3afe5
keemHwa <23390@naver.com>
2023-10-18 06:44:12 +0900

    :bug: #10 cookie 관련 오류 수정

--- config.js
+++ config.js
@@ -16,7 +16,8 @@ const config = {
     saveUninitialized: true,
     cookie: {
       httpOnly: true,
-      // secure:true, //https only
+      sameSite: "none",
+      secure: true,
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
